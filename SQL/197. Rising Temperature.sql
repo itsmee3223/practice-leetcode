3975f771cc5dca413baeb5d73a02cd4b3e0be7c9
itsmee3223 <tamvam.pemberani@gmail.com>
2023-01-17 13:34:42 +0700

    SQL - Day 6

---
+++ SQL/197. Rising Temperature.sql
@@ -0,0 +1,67 @@
+-- SQL SCHEMA
+
+-- Create table If Not Exists Weather (id int, recordDate date, temperature int)
+-- Truncate table Weather
+-- insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+-- insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+-- insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+-- insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+
+-- Table: Weather
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | recordDate    | date    |
+-- | temperature   | int     |
+-- +---------------+---------+
+-- id is the primary key for this table.
+-- This table contains information about the temperature on a certain day.
+ 
+
+-- Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Weather table:
+-- +----+------------+-------------+
+-- | id | recordDate | temperature |
+-- +----+------------+-------------+
+-- | 1  | 2015-01-01 | 10          |
+-- | 2  | 2015-01-02 | 25          |
+-- | 3  | 2015-01-03 | 20          |
+-- | 4  | 2015-01-04 | 30          |
+-- +----+------------+-------------+
+-- Output: 
+-- +----+
+-- | id |
+-- +----+
+-- | 2  |
+-- | 4  |
+-- +----+
+-- Explanation: 
+-- In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+-- In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+-- ==============================================================================
+-- Write your MySQL query statement below
+select cur.id
+from Weather cur
+join Weather prev
+on prev.recordDate + interval 1 day = cur.recordDate
+where cur.temperature > prev.temperature
+;
+
+-- ==============================================================================
+select w1.id
+from Weather w1, Weather w2
+where w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY) and w1.temperature > w2.temperature;
+;
