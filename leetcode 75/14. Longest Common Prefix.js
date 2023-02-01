8647ddd458e8e2874b422fe32481555a5a2372a7
itsmee3223 <tamvam.pemberani@gmail.com>
2023-02-01 10:13:45 +0700

    Level 2 - 14. Longest Common Prefix

---
+++ leetcode 75/14. Longest Common Prefix.js
@@ -0,0 +1,48 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters.
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (!strs.length) return "";
+
+  for (let i = 0; i <= strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+
+  return strs[0];
+};
+
+var longestCommonPrefix = (str) => {
+  if (str === undefined || str.length === 0) return "";
+
+  let i = 0;
+  return str.reduce((prev, current) => {
+    while (prev[i] && current[i] && prev[i] === current[i]) i++;
+
+    return prev.slice(0, i);
+  });
+};
