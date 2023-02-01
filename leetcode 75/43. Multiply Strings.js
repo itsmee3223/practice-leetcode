366405aefaa3f43b71a1f38e55185454ff0ec1b6
itsmee3223 <tamvam.pemberani@gmail.com>
2023-02-01 10:13:36 +0700

    Level 2 - 43. Multiply Strings

---
+++ leetcode 75/43. Multiply Strings.js
@@ -0,0 +1,44 @@
+// Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+// Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+// Example 1:
+
+// Input: num1 = "2", num2 = "3"
+// Output: "6"
+// Example 2:
+
+// Input: num1 = "123", num2 = "456"
+// Output: "56088"
+
+// Constraints:
+
+// 1 <= num1.length, num2.length <= 200
+// num1 and num2 consist of digits only.
+// Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function (num1, num2) {
+  if (num1 === "0" || num2 === "0") return "0";
+  const m = num1.length,
+    n = num2.length,
+    res = new Array(m + n).fill(0);
+
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      const p1 = i + j,
+        p2 = 1 + i + j;
+
+      let sum = res[p2] + Number(num1[i]) * Number(num2[j]);
+      res[p2] = sum % 10;
+      res[p1] += Math.floor(sum / 10);
+    }
+  }
+
+  if (res[0] === 0) res.shift();
+  return res.join("");
+};
