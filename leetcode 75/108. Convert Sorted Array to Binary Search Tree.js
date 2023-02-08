14061361d8baf09a941189ee4b14395eb9999265
itsmee3223 <tamvam.pemberani@gmail.com>
2023-02-08 10:23:36 +0700

    108. Convert Sorted Array to Binary Search Tree

---
+++ leetcode 75/108. Convert Sorted Array to Binary Search Tree.js
@@ -0,0 +1,59 @@
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a
+// height-balanced
+//  binary search tree.
+
+// Example 1:
+
+// Input: nums = [-10,-3,0,5,9]
+// Output: [0,-3,9,-10,null,5]
+// Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+// Example 2:
+
+// Input: nums = [1,3]
+// Output: [3,1]
+// Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums is sorted in a strictly increasing order.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  const createBst = (left, right) => {
+    if (left > right) return null;
+    const mid = Math.floor((left + right) / 2);
+    const currentNode = new TreeNode(nums[mid]);
+    currentNode.left = createBst(left, mid - 1);
+    currentNode.right = createBst(mid + 1, right);
+    return currentNode;
+  };
+  return createBst(0, nums.length - 1);
+};
+
+var sortedArrayToBST = function (nums) {
+  const helper = (nums, left, right) => {
+    if (left >= right) return null;
+    const mid = Math.floor((left + right) / 2);
+    return new TreeNode(
+      nums[mid],
+      helper(nums, left, mid),
+      helper(nums, mid + 1, right)
+    );
+  };
+
+  return helper(nums, 0, nums.length);
+};
