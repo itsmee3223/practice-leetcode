f39d07e82da1952a9e6687d5cd25d3c46c4304ef
itsmee3223 <tamvam.pemberani@gmail.com>
2023-04-04 09:32:19 +0700

    350. Intersection of Two Arrays II

---
+++ Top Interview Question/350. Intersection of Two Arrays II.js
@@ -0,0 +1,33 @@
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  const map = new Map();
+  for (const n of nums1) {
+    map.set(n, map.get(n) + 1 || 1);
+  }
+
+  const result = [];
+  for (const n of nums2) {
+    if (map.has(n) && map.get(n) > 0) {
+      result.push(n);
+      map.set(n, map.get(n) - 1);
+    }
+  }
+
+  return result;
+};
